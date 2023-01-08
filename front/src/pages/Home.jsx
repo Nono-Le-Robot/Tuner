6665ba27d977bd6e24e5442171872aa41ee5dde9
NonoLeRobot <sannier.renaud@gmail.com>
2023-01-08 20:28:18 +0100

    blank project

---
+++ front/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+
+export default function Home() {
+  //JS HERE
+
+  return (
+    <>
+      <Container>BLANK FRONT PROJECT</Container>
+    </>
+  );
+}
+
+const Container = styled.div``;
